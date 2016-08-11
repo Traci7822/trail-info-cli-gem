51970f87a45d8f06e3e83cb36369fc9243ded341
Traci7822 <tracir@gmail.com>
2016-08-10 21:25:44 -0600

    State list is putting to the screen

--- lib/trail_info/CLI.rb
+++ lib/trail_info/CLI.rb
@@ -9,28 +9,44 @@ class TrailInfo::CLI
   end
 
   def menu
-    puts "What Country would you like to find a trail in?"
+    puts "What state would you like to find a trail in?"
     pick_location
     display_trails
   end
   
   def pick_location
-    countries = ["United States", "United Kingdon", "Germany", "Japan"]
-    puts "Please select a number to find a trail in that country then press enter."
-    countries.each_with_index { |country, index| puts "#{index + 1}: #{country}"}
-    country_selection = gets.chomp
-    puts "You've selected #{country_selection}: #{countries[country_selection.to_i - 1]}. Please select a state:"
+    #populate locations hash using nokogiri
 
-    states = ["California", "New York", "Texas", "Washington"]
-    states.each_with_index { |state, index| puts "#{index + 1}: #{state}"}
-    state_selection = gets.chomp
-    puts "You've selected #{state_selection}: #{states[state_selection.to_i - 1]}. Please select a city or enter 'go back' to return to the previous screen:"
+    locations = {
+    "California" => ["San Diego", "Los Angeles", "San Francisco", "San Jose"],
+    "New York" => [],
+    "Texas" => [],
+    "Washington" => []
+    }
+    
+    @state_list = Hash.new
+    @city_list = Hash.new
+    
+    locations.each_with_index do |state, index|
+        @state_list = {(index + 1) => state}
+        puts "#{@state_list.keys}. #{@state_list.values[0][0]}"
+        end
+   
 
-      #State List
-      #ask for user input to select a state
-      #go back option
+   puts "You've selected #{state_selection}: #{states[state_selection.to_i - 1]}. Please select a city or enter 'go back' to return to the previous screen:"     #cities_array.each_with_index do |city, index| 
+        #@city_list = {(index + 1) => city}
+      #end
+    #end
 
-        #City List
+    #states.each_with_index { |state, index| puts "#{index + 1}: #{state}"}
+    #state_selection = gets.chomp
+    
+     
+    #direct towards city array/hash for state selection
+    #populate cities hash w/ scraping 
+
+    city_selection = gets.chomp
+    puts "You've selected #{@city_list[city_selection.to_i - 1]}. Here are the available trails in #{@city_list[city_selection.to_i - 1]}"
         #ask for user input to select city
         #go back option
   end
