e2ee53ad872b1bfc57d2cbe6418814cbad852184
Traci7822 <tracir@gmail.com>
2016-08-11 08:52:59 -0600

    added trail attributes

--- lib/trail_info/CLI.rb
+++ lib/trail_info/CLI.rb
@@ -36,8 +36,29 @@ class TrailInfo::CLI
     end
 
     state_selection = gets.chomp
-    puts "You've selected #{state_selection}. #{@states[state_selection.to_i - 1]}. Please select a city or enter 'go back' to return to the previous screen:"
+    puts "You've selected #{state_selection}. #{@states[state_selection.to_i - 1]}. Here are the best trails in #{@states[state_selection.to_i - 1]} according to EveryTrail.com. Enter 'reset' to go back to the menu."
+    display_trails
+    input = gets.chomp
+    if input == "reset"
+      menu
+    end
   end
+
+
+  def display_trails
+    new_trail = TrailInfo::Trails.new
+    new_trail.name = "Pine Ridge Trail to Sykes Hot Springs"
+    new_trail.difficulty = "Difficult"
+    new_trail.length = "20 miles"
+    new_trail.duration = "Multiple Days"
+    new_trail.location = "Pfeiffer Big Sur State Park"
+    binding.pry
+    #Trail list w/ attributes
+    #go back or reset option
+  end
+end
+
+=begin
 
   def pick_city
     @state_arrays = {}
@@ -58,10 +79,19 @@ class TrailInfo::CLI
     puts "You've selected #{city_selection}. #{@cities[city_selection.to_i - 1]}. Here are the available trails in #{@cities[city_selection.to_i - 1]}." 
   end
 
-  def display_trails
-    #Trail list w/ attributes
-    #option to select for more info
-    #go back or reset option
-  end
-
 end
+
+
+pick_city:
+1. Alabama:
+  1. Natural Wells Trail - Montesano State Park: Length: 6.2 miles, Duration: 4 hours, 
+2. Alaska:
+  1. East Fork Trail and Tulchina Falls: Difficulty: Moderate, Length: 6.3 miles, Duration: Half day
+3. Arizona:
+  
+
+Trails
+
+Difficulty, Length, Duration, Notes
+
+=end
--- lib/trail_info/trails.rb
+++ lib/trail_info/trails.rb
@@ -1,5 +1,5 @@
 class TrailInfo::Trails
 
-
+attr_accessor :name, :difficulty, :length, :duration, :location
 
 end
