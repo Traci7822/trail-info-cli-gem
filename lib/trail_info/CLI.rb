a84fe7a163016f8607f9eded365d53bc6da14970
Traci7822 <tracir@gmail.com>
2016-08-10 20:04:01 -0600

    added state selection part of pick_location method

--- lib/trail_info/CLI.rb
+++ lib/trail_info/CLI.rb
@@ -19,7 +19,12 @@ class TrailInfo::CLI
     puts "Please select a number to find a trail in that country then press enter."
     countries.each_with_index { |country, index| puts "#{index + 1}: #{country}"}
     country_selection = gets.chomp
-    
+    puts "You've selected #{country_selection}: #{countries[country_selection.to_i - 1]}. Please select a state:"
+
+    states = ["California", "New York", "Texas", "Washington"]
+    states.each_with_index { |state, index| puts "#{index + 1}: #{state}"}
+    state_selection = gets.chomp
+    puts "You've selected #{state_selection}: #{states[state_selection.to_i - 1]}. Please select a city or enter 'go back' to return to the previous screen:"
 
       #State List
       #ask for user input to select a state
