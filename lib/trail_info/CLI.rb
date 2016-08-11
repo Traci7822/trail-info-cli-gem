3996a6a76d723598d12a9c4adff541118912f217
Traci7822 <tracir@gmail.com>
2016-08-11 10:04:44 -0600

    displaying trail names from trails_array

--- lib/trail_info/CLI.rb
+++ lib/trail_info/CLI.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class TrailInfo::CLI
-
+    
+  
 
   def call
     puts "Welcome to Trail Finder!"
@@ -16,82 +17,39 @@ class TrailInfo::CLI
   
   def pick_location
     #populate locations hash using nokogiri
-
-    @locations = {
-    "California" => ["San Diego", "Los Angeles", "San Francisco", "San Jose"],
-    "New York" => [],
-    "Texas" => [],
-    "Washington" => []
-    }
-
+    @states = ["California", "New York", "Texas", "Washington"]
+    @states.each do |state|
+      #need to create arrays for each state in which to house all of the states trails
+    end
     pick_state
-    pick_city
   end
 
   def pick_state
-    @states = []
-    @locations.keys.each_with_index do |state, i|
-      @states << state
+    @trails = ["trail1", "trail2", "trail3"]
+
+
+    @states.each_with_index do |state, i|
       puts "#{i + 1}. #{state}"
     end
 
     state_selection = gets.chomp
-    puts "You've selected #{state_selection}. #{@states[state_selection.to_i - 1]}. Here are the best trails in #{@states[state_selection.to_i - 1]} according to EveryTrail.com. Enter 'reset' to go back to the menu."
-    display_trails
+    puts "You've selected #{state_selection}. #{@states[state_selection.to_i - 1]}. Here are the 20 best trails in #{@states[state_selection.to_i - 1]} according to EveryTrail.com. Enter 'reset' to go back to the menu."
+    display_trails(@trails)
+
     input = gets.chomp
     if input == "reset"
       menu
     end
-  end
-
-
-  def display_trails
-    new_trail = TrailInfo::Trails.new
-    new_trail.name = "Pine Ridge Trail to Sykes Hot Springs"
-    new_trail.difficulty = "Difficult"
-    new_trail.length = "20 miles"
-    new_trail.duration = "Multiple Days"
-    new_trail.location = "Pfeiffer Big Sur State Park"
-    binding.pry
-    #Trail list w/ attributes
-    #go back or reset option
-  end
-end
-
-=begin
-
-  def pick_city
-    @state_arrays = {}
-    @cities = []
-    @locations.keys.each do |state| #redundant. this recreates @locations but I like this version better
-      @state_arrays[state] = @cities
-  end
-  #need to correlate city arrays with state selection. all states are populated with california cities now. 
-
-    @locations.values.each do |city|
-      city.each_with_index do |city, i|
-        @cities << city
-        puts "#{i + 1}. #{city}"
-      end
-    end
-
-    city_selection = gets.chomp
-    puts "You've selected #{city_selection}. #{@cities[city_selection.to_i - 1]}. Here are the available trails in #{@cities[city_selection.to_i - 1]}." 
-  end
-
-end
-
-
-pick_city:
-1. Alabama:
-  1. Natural Wells Trail - Montesano State Park: Length: 6.2 miles, Duration: 4 hours, 
-2. Alaska:
-  1. East Fork Trail and Tulchina Falls: Difficulty: Moderate, Length: 6.3 miles, Duration: Half day
-3. Arizona:
   
+  end
 
-Trails
 
-Difficulty, Length, Duration, Notes
+  def display_trails(trails_array)
+    TrailInfo::Trails.create_trails(trails_array)
+    TrailInfo::Trails.all.each_with_index do |trail, index|
+      puts "#{index + 1}. #{trail.name}"
+    end
+  end
+end
 
-=end
+ 
--- lib/trail_info/trails.rb
+++ lib/trail_info/trails.rb
@@ -2,4 +2,30 @@ class TrailInfo::Trails
 
 attr_accessor :name, :difficulty, :length, :duration, :location
 
+@@all = []
+
+def initialize
+
 end
+
+def self.create_trails(trails_array)
+  trails_array.each do |trail|
+    trail = self.new
+    trail.name = "Pine Ridge Trail to Sykes Hot Springs"
+    trail.difficulty = "Difficult"
+    trail.length = "20 miles"
+    trail.duration = "Multiple Days"
+    location = "Pfeiffer Big Sur State Park"
+    @@all << trail
+  end
+end
+
+def self.all
+  @@all
+end
+
+end
+
+
+
+
