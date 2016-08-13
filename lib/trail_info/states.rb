5c6d21d089f4e5ed26f402402627b414553f8f3f
Traci7822 <tracir@gmail.com>
2016-08-12 23:39:41 -0600

    trail lengths and trail names populated to state array

--- lib/trail_info/states.rb
+++ lib/trail_info/states.rb
@@ -21,24 +21,24 @@ attr_accessor :name
   def scrape_guides
     @doc.css(".activityTrail").css(".trailmeta").each do |scrape|
       new_trail = TrailInfo::Trail.new
-      trail_name = scrape.css("a").text.split("\r\n                                                    ").join unless scrape.css("a").text.split("\r\n                                                    ") == ""     
-
-      scrape.css("p")[0].children.each do |section|
-        if section.include?("miles")
-          @trail_length = scrape.css("p")[0].children[6].text.split("\r\n").join.strip.gsub(/\s+/, " ").join
+      trail_name = scrape.css("a").text.split("\r\n                                                    ").join unless scrape.css("a").text.split("\r\n                                                    ") == ""
+      
+      scrape.css("p").each do |section|
+        if section.text.include?("miles")
+          @trail_length = [section.text.split[3], section.text.split[4]].join(" ")
         else
-          @trail_length = nil
-        end
-        
-        scrape.css("p").children.each do |section|
-          if section.include?("Surface:")
-            @trail_surface = scrape.css("p")[1].children[10].text.split("\r\n").join.strip
-          else
-            @trail_surface = ""
-          end
+          @trail_length = ""
         end
       end
-
+        
+      scrape.css("p").children.each do |section|
+        if section.include?("Surface:")
+          @trail_surface = scrape.css("p")[1].children[10].text.split("\r\n").join.strip
+        else
+          @trail_surface = ""
+        end
+      end
+      
       new_trail.name = trail_name            
       new_trail.length = @trail_length
       new_trail.surface = @trail_surface
