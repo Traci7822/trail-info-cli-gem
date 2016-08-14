34518dc2c37e29e069ece4717f5196b0d681ea68
Traci7822 <tracir@gmail.com>
2016-08-14 13:51:35 -0600

    broke scraper into trail attribute methods

--- lib/trail_info/scraper.rb
+++ lib/trail_info/scraper.rb
@@ -9,25 +9,39 @@ class TrailInfo::Scraper
   end
 
   def self.scrape_state_guides(state_code) #fix sandwich code
-    trail_array = []
     @state_scrape = Nokogiri::HTML(open("http://www.traillink.com/stateactivity/#{state_code}-hiking-trails.aspx"))
-    @state_scrape.css(".activityTrail").css(".trailmeta").each do |state_scrape|
-      new_trail = {}
-      new_trail["name"] = state_scrape.css("a").text.split.join(" ") unless state_scrape.css("a").text.split.join(" ") == ""
-      state_scrape.css("p").each do |trail_section|
-        if trail_section.text.include?("miles") ? new_trail["length"] = [trail_section.text.split[3], trail_section.text.split[4]].join(" ") : new_trail["length"] = ""
-        end
+    scrape_attributes
+    @trail_array
+  end
 
-        if trail_section.text.include?("Surface:")
-          if trail_section.text.split[7] == nil ? new_trail["surface"] = trail_section.text.split[6] : new_trail["surface"] = [trail_section.text.split[6], trail_section.text.split[7].gsub(/,/, "")].join(" ")
-          end
-        else
-          new_trail["surface"] = ""
-        end
-      end
-      trail_array << new_trail
+  def self.scrape_attributes
+    @trail_array = []
+    @state_scrape.css(".activityTrail").css(".trailmeta").each do |state_scrape|
+      scrape_trail_attributes(state_scrape)
+      @trail_array << @new_trail
     end
-    trail_array
+  end
+
+  def self.scrape_trail_attributes(state_scrape)
+    @new_trail = {}
+    @new_trail["name"] = state_scrape.css("a").text.split.join(" ") unless state_scrape.css("a").text.split.join(" ") == ""
+    state_scrape.css("p").each do |trail_section|
+      trail_length_and_surface(trail_section)
+      @new_trail
+    end
+  end
+
+  def self.trail_length_and_surface(trail_section)
+    if trail_section.text.include?("miles") ? @new_trail["length"] = [trail_section.text.split[3], trail_section.text.split[4]].join(" ") : @new_trail["length"] = ""
+    end
+
+    if trail_section.text.include?("Surface:")
+      if trail_section.text.split[7] == nil ? @new_trail["surface"] = trail_section.text.split[6] : @new_trail["surface"] = [trail_section.text.split[6], trail_section.text.split[7].gsub(/,/, "")].join(" ")
+      end
+    else
+      @new_trail["surface"] = ""
+    end
+    @new_trail
   end
 
 end
