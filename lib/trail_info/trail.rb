59107af4147851365a6ad3ad19f1b02d0393d02b
Traci7822 <tracir@gmail.com>
2016-08-15 14:06:20 -0600

    added comments to trail

--- lib/trail_info/trail.rb
+++ lib/trail_info/trail.rb
@@ -4,18 +4,18 @@ attr_accessor :name, :length, :surface, :state, :synopsis, :url
 
 @@all = []
 
-def initialize(state, trail_attributes)
+def initialize(state, trail_attributes) #Creates new trail instance
   trail_attributes.each do |attribute_name, attribute_value|
     self.send("#{attribute_name}=", attribute_value)
   end
   save
 end
 
-def save
+def save #saves trail instance
   @@all << self
 end
 
-def self.all
+def self.all #returns all trail instances
   @@all
 end
 
