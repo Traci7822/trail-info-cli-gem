3c7e90a5f33ffb3427df9c4ec2e3b8dcd17f5e1b
Traci7822 <tracir@gmail.com>
2016-08-14 19:23:22 -0600

    consolidated CLI and User Interaction class

--- lib/trail_info/trail.rb
+++ lib/trail_info/trail.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class TrailInfo::Trail
 
 attr_accessor :name, :length, :surface, :state, :synopsis, :url
@@ -10,7 +9,7 @@ def initialize(state, trail_attributes)
     self.send("#{attribute_name}=", attribute_value)
   end
   save
-  end
+end
 
 def save
   @@all << self
@@ -19,22 +18,5 @@ end
 def self.all
   @@all
 end
-
-#def self.print_all
-#  all.each_with_index do |trail, index|
-#    puts "#{i}. #{trail.name} | Length: #{trail.length} | Surface: #{trail.surface}|"
-#  end
-#end
-
-#def self.trails_by_state(state)
- # trail_list = []
- # self.all.each do |trail|
- #   if trail.state.name == selected_state.name
- #     trail_list << trail
-#    end
- # end
-#end
-
-
 
 end
