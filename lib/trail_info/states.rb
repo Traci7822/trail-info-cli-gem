969a9c3919e93b598f59a2e09ea3af9d8efc8866
Traci7822 <tracir@gmail.com>
2016-08-15 14:05:45 -0600

    added comments to state

--- lib/trail_info/states.rb
+++ lib/trail_info/states.rb
@@ -3,7 +3,7 @@ class TrailInfo::State
   attr_accessor :name, :trails
   @@all = []
 
-  def initialize(name, state_code)
+  def initialize(name, state_code) #Creates new state instance and initializes scraping and new trail instances
     @name = name
     @trails = []
     TrailInfo::Scraper.new(state_code)
@@ -13,15 +13,15 @@ class TrailInfo::State
     @@all << self
   end
 
-  def self.all
+  def self.all #returns all trails for state instance
     @@all
   end
 
-  def self.clear_all
+  def self.clear_all #clears trails for state instance
     @@all.clear
   end
 
-  def self.trail_list
+  def self.trail_list #Writes list of states trails
     @trail_list = []
     @@all[0].trails.each.with_index(1) do |trail, i| 
       trail_description = "#{i}. #{trail.name} | Length: #{trail.length} | Surface: #{trail.surface}|"
