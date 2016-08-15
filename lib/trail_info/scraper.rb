7e7a610adb1a09e3b6ccf919390aad1ecbf9b918
Traci7822 <tracir@gmail.com>
2016-08-15 14:19:30 -0600

    added comments to scraper

--- lib/trail_info/scraper.rb
+++ lib/trail_info/scraper.rb
@@ -1,20 +1,20 @@
 class TrailInfo::Scraper
 
-  def initialize(state_code)
+  def initialize(state_code) #Creates new scraper instance with state code
     @state_code = state_code
   end
 
-  def self.create_trails(state_code)
+  def self.create_trails(state_code) #Initiates state scrape
     scrape_state_guides(state_code)
   end
 
-  def self.scrape_state_guides(state_code) #fix sandwich code
+  def self.scrape_state_guides(state_code) #Scrapes state site, collects and returns trail information
     @state_scrape = Nokogiri::HTML(open("http://www.traillink.com/stateactivity/#{state_code}-hiking-trails.aspx"))
     scrape_attributes
     @trail_array
   end
 
-  def self.scrape_attributes
+  def self.scrape_attributes #Scrapes trail attributes and collects to trail_array
     @trail_array = []
     @state_scrape.css(".activityTrail").css(".trailmeta").each do |state_scrape|
       scrape_trail_attributes(state_scrape)
@@ -22,7 +22,7 @@ class TrailInfo::Scraper
     end
   end
 
-  def self.scrape_trail_attributes(state_scrape)
+  def self.scrape_trail_attributes(state_scrape) #Scrapes trail details
     @new_trail = {}
     @new_trail["synopsis"] = scrape_synopsis(state_scrape)
     @new_trail["name"] = scrape_trail_name(state_scrape)
@@ -33,11 +33,11 @@ class TrailInfo::Scraper
     end
   end
 
-  def self.scrape_trail_name(state_scrape)
+  def self.scrape_trail_name(state_scrape) #Scrapes trail name
     state_scrape.css("a").text.split.join(" ") unless state_scrape.css("a").text.split.join(" ") == ""
   end
 
-  def self.scrape_trail_length_and_surface(trail_section)
+  def self.scrape_trail_length_and_surface(trail_section) #Scrapes trail length and surface information
     if trail_section.text.include?("miles") ? @new_trail["length"] = [trail_section.text.split[3], trail_section.text.split[4]].join(" ") : @new_trail["length"] = ""
     end
 
@@ -50,11 +50,11 @@ class TrailInfo::Scraper
     @new_trail
   end
 
-  def self.scrape_synopsis(state_scrape)
+  def self.scrape_synopsis(state_scrape) #Scrapes trail synopsis
     @state_synopsis = state_scrape.css(".synopsis").text.strip
   end
 
-  def self.scrape_url(state_scrape)
+  def self.scrape_url(state_scrape) #Scrapes trail url
     path = state_scrape.css("a").attribute("href").value
     state_url = "http://www.traillink.com#{path}"
   end
